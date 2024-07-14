18a7d7887d68a9f6f725fd4d9e669d4b5c5a8794
Attila Krajcsi <krajcsi@users.noreply.github.com>
2024-07-14 17:33:15 +0200

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set -ev
+
+echo "sdfsfsdfsdfswdfsfs"
+
 # rm -rf ./docs
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', output_dir='./docs_test')"
